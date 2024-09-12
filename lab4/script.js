03bcb2b0e24cd02451b49bd218fdb97010fab978
OlhaMish <olgami1402@gmail.com>
2024-09-12 19:44:34 +0300

    feat: add line selection

---
+++ lab4/script.js
@@ -0,0 +1,22 @@
+function toggleBackground(element, color1, color2, class1, class2) {
+    const currentBackgroundColor = getComputedStyle(element).backgroundColor;
+
+    if (currentBackgroundColor === color1) {
+        element.classList.remove(class1);
+        element.classList.add(class2);
+    } else {
+        element.classList.remove(class2);
+        element.classList.add(class1);
+    }
+}
+
+const book9 = document.getElementById('9');
+book9.addEventListener('click', function() {
+    toggleBackground(this, 'rgb(150, 255, 200)', 'rgb(150, 150, 255)', 'active_1', 'active_2');
+});
+
+const secondItem = document.querySelector('ol li:nth-child(2)');
+secondItem.addEventListener('click', function() {
+    toggleBackground(this, 'rgb(150, 150, 255)', 'rgb(150, 255, 200)', 'active_2', 'active_1');
+});
+
