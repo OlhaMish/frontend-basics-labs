c95bc7ee01932621afa2548e8cb23dbc08129058
OlhaMish <olgami1402@gmail.com>
2024-09-21 11:39:22 +0300

    refactor: remove all unnecessary files from src directory

--- lab7/src/App.js
+++ lab7/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "./components/Header"; // Corrected import path
-import Content from "./components/Content"; // Corrected import path
+import Header from "./components/Header";
+import Content from "./components/Content";
 import "./style.css";
 
 const App = () => {
   return (
-    <div style={{ marginLeft: "10px" }}>
+    <div style={{ marginLeft: "2px" }}>
       <Header />
       <Content />
     </div>
