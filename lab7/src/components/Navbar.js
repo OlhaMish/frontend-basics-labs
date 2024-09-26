3568adfc8950db5fa343ab0efaa76fbdd30bba5c
OlhaMish <olgami1402@gmail.com>
2024-09-26 13:46:03 +0300

    refactor: change li names

--- lab7/src/components/Navbar.js
+++ lab7/src/components/Navbar.js
@@ -5,10 +5,10 @@ const Navbar = () => {
     <nav>
       <ul>
         <li>
-          <Link to="/">Головна сторінка</Link>
+          <Link to="/">Biography</Link>
         </li>
         <li>
-          <Link to="/gallery">Галерея товарів</Link>
+          <Link to="/gallery">Goods Gallery</Link>
         </li>
       </ul>
     </nav>
